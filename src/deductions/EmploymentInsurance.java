6ce98cfca76952a8b4e1341cb88048eb7863ceac
Ahmad Khanafer <ahmad.khanafer.191@gmail.com>
2025-03-17 12:49:21 -0400

    t

--- src/deductions/EmploymentInsurance.java
+++ src/deductions/EmploymentInsurance.java
@@ -6,7 +6,10 @@ public class EmploymentInsurance extends Deductions{
 
     @Override
     public void calculateTax(Employee emp) {
-
+        double deduction;
+        deduction = emp.getAnnualGrossSalary() * 0.0164;
+        emp.setDeductions( emp.getDeductions() + Math.min(deduction, 1077.48));
+        System.out.println("Deductions emplymentInsurance: " + emp.getDeductions());
     }
 }
 
